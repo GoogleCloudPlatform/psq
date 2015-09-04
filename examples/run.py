c8b8e1dcd6147684a551f429f994077fd2c05054
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-09-04 09:48:56 -0700

    Adding travis configuration
    Change-Id: Id570409c60e0602e3799bdfdab10ecaebf52df21

--- examples/run.py
+++ examples/run.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gcloud import pubsub, datastore
+from gcloud import datastore, pubsub
 import psq
 import tasks
 
